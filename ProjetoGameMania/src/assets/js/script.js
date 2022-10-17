36fe5b1aae46ad6cd93a7d90aad6a0da94a016b0
Alexandre Hoss <104204035+Axionh@users.noreply.github.com>
2022-10-17 09:33:54 -0300

    Delete ProjetoGameMania directory

--- ProjetoGameMania/src/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-function menulateralmostrar(){
-    let navbar = document.getElementById("navMenu");
-    navbar.classList.toggle("menu-lateral-mostrar")
-    
-    
-    console.log (navbar.classList)
-}
