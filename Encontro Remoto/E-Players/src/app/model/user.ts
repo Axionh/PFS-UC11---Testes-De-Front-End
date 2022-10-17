f866e983b5b0cc0a5f316d9b4d9d997448f75c8d
Alexandre Hoss <104204035+Axionh@users.noreply.github.com>
2022-10-17 09:34:14 -0300

    Delete Encontro Remoto directory

--- Encontro Remoto/E-Players/src/app/model/user.ts
+++
@@ -1,8 +0,0 @@
-export class User {
-    constructor (
-        public nome?: string,
-        public idade?: string,
-        public email?: string,
-        public password?: string,
-    ){}
-}
--- Encontro Remoto/E-Players/src/assets/js/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarMenu() {
-    // DOM JAVASCRIPT
-    let navbar = document.getElementById("navMenu");
-    navbar.classList.toggle("mostraMenu")
-    
-    console.log(navbar.classList);
-}
